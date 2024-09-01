a9cdda39941b54848a50a111a17ce918278b87c7
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-09-01 01:18:01 -0400

    Crud-API

---
+++ src/test/java/com/example/_972/CRUD/API/CrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._972.CRUD.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
